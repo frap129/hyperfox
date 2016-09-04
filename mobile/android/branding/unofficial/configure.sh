04c03d62b7f59d5a4ba9b3b27b1ddc44c18a999d
Joe Maples <joe@frap129.org>
2016-09-04 02:47:27 +0000

    Rebase: Updates for Hyperfox
    Signed-off-by: Joe Maples <joe@frap129.org>

--- mobile/android/branding/unofficial/configure.sh
+++ mobile/android/branding/unofficial/configure.sh
@@ -2,8 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ANDROID_PACKAGE_NAME=org.frap129.saberfox
-MOZ_APP_DISPLAYNAME="SaberFox"
+ANDROID_PACKAGE_NAME=org.frap129.hyperfox
+MOZ_APP_DISPLAYNAME="Hyperfox"
 MOZ_UPDATER=
 MOZ_ANDROID_ANR_REPORTER=
 MOZ_ANDROID_GCM_SENDERID=965234145045
