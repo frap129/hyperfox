e997dd922e6f2c0bb7108d2d73a61c1d5ff2a590
Joe Maples <joe@maples.dev>
2024-11-26 18:39:26 -0500

    build: Improve container build

--- builder/build-hyperfox.sh
+++ builder/build-hyperfox.sh
@@ -1,9 +1,42 @@
 #!/usr/bin/env bash
 
+srcdir="/src/librewolf-$(cat /src/version)-$(cat /src/release)"
+export MACH_BUILD_PYTHON_NATIVE_PACKAGE_SOURCE=pip
+export MOZBUILD_STATE_PATH="$srcdir/mozbuild"
+export MOZ_BUILD_DATE="$(date -u${SOURCE_DATE_EPOCH:+d @$SOURCE_DATE_EPOCH} +%Y%m%d%H%M%S)"
+export MOZ_NOSPAM=1
+
 cd /src || exit
-make dir
-make bootstrap
-dbus-run-session \
-  xvfb-run -s "-screen 0 1920x1080x24 -nolisten local" \
-  bash -c "make build || make build"
-make package
+make dir || exit
+make bootstrap || exit
+
+# Build without PGO
+cat >$srcdir/mozconfig assets/mozconfig.new - <<END
+ac_add_options --enable-profile-generate=cross
+ac_add_options --disable-elf-hack
+ac_add_options --enable-linker=mold
+END
+make build || exit
+
+# Generate PGO profdata
+(
+  cd $srcdir || exit
+  ./mach package
+  LLVM_PROFDATA="$MOZBUILD_STATE_PATH/clang/bin/llvm-profdata" JARLOG_FILE="$srcdir/jarlog" \
+    dbus-run-session \
+    xvfb-run -s "-screen 0 1920x1080x24 -nolisten local" \
+    ./mach python build/pgo/profileserver.py
+  ./mach clobber objdir
+)
+
+# Final build
+cat >$srcdir/mozconfig assets/mozconfig.new - <<END
+ac_add_options --enable-elf-hack=relr
+ac_add_options --enable-linker=lld
+ac_add_options --enable-lto=cross,full
+ac_add_options --enable-profile-use=cross
+ac_add_options --with-pgo-profile-path=$srcdir/merged.profdata
+ac_add_options --with-pgo-jarlog=$srcdir/jarlog
+END
+make build || exit
+make package || exit
