ab2662227c11f9a9443c7423fffb70588643c0cb
any1here <any1here@duck.com>
2026-03-30 21:11:08 +0200

    Move our settings to the new config based approach

--- patches/pref-pane/librewolf.js
+++ patches/pref-pane/librewolf.js
@@ -44,120 +44,111 @@ Preferences.addAll([
   { id: "toolkit.legacyUserProfileCustomizations.stylesheets", type: "bool" },
 ]);
 
-var gLibrewolfPane = {
-  _pane: null,
-
-  // called when the document is first parsed
-  init() {
-    this._pane = document.getElementById("paneLibrewolf");
-
-    // Set all event listeners on checkboxes
-    setBoolSyncListeners(
-      "librewolf-extension-update-checkbox",
-      ["extensions.update.autoUpdateDefault", "extensions.update.enabled"],
-      [true,                                  true                       ],
-    );
-    setBoolSyncListeners(
-      "librewolf-ipv6-checkbox",
-      ["network.dns.disableIPv6"],
-      [false,                   ],
-    );
-    setBoolSyncListeners(
-      "librewolf-sync-checkbox",
-      ["identity.fxaccounts.enabled"],
-      [true,                   ],
-    );
-    setBoolSyncListeners(
-      "librewolf-autocopy-checkbox",
-      ["clipboard.autocopy", "middlemouse.paste"],
-      [true,                 true               ],
-    );
-    setBoolSyncListeners(
-      "librewolf-styling-checkbox",
-      ["toolkit.legacyUserProfileCustomizations.stylesheets"],
-      [true,                                                ],
-    );
-    setBoolSyncListeners(
-      "librewolf-webgl-checkbox",
-      ["librewolf.webgl.prompt", "webgl.disabled"],
-      [true,                     false           ],
-    );
-    setBoolSyncListeners(
-      "librewolf-webgl-prompt-checkbox",
-      ["librewolf.webgl.prompt.hide"],
-      [true                         ],
-    );
-    setBoolSyncListeners(
-      "librewolf-rfp-checkbox",
-      ["privacy.resistFingerprinting"],
-      [true                          ],
-    );
-
-    setBoolSyncListeners(
-      "librewolf-letterboxing-checkbox",
-      ["privacy.resistFingerprinting.letterboxing"],
-      [true                                       ],
-    );
-
-    setSyncListeners(
-      "librewolf-goog-safe-checkbox",
-      [
-        "browser.safebrowsing.malware.enabled",
-        "browser.safebrowsing.phishing.enabled",
-        "browser.safebrowsing.blockedURIs.enabled",
-        "browser.safebrowsing.provider.google4.gethashURL",
-        "browser.safebrowsing.provider.google4.updateURL",
-        "browser.safebrowsing.provider.google.gethashURL",
-        "browser.safebrowsing.provider.google.updateURL",
-      ],
-      [
-        true,
-        true,
-        true,
-        "https://safebrowsing.googleapis.com/v4/fullHashes:find?$ct=application/x-protobuf&key=%GOOGLE_SAFEBROWSING_API_KEY%&$httpMethod=POST",
-        "https://safebrowsing.googleapis.com/v4/threatListUpdates:fetch?$ct=application/x-protobuf&key=%GOOGLE_SAFEBROWSING_API_KEY%&$httpMethod=POST",
-        "https://safebrowsing.google.com/safebrowsing/gethash?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2",
-        "https://safebrowsing.google.com/safebrowsing/downloads?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2&key=%GOOGLE_SAFEBROWSING_API_KEY%",
-      ],
-      [
-        false,
-        false,
-        false,
-        "",
-        "",
-        "",
-        "",
-      ]
-    );
-
-    setXOriginPolicySyncListeners(
-      "librewolf-xorigin-ref-checkbox",
-      "network.http.referer.XOriginPolicy",
-      [1, 2],
-      [0]
-    );
-
-    // Set event listener on open profile directory button
-    setEventListener("librewolf-open-profile", "command", openProfileDirectory);
-    // Set event listener on open about:config button
-    setEventListener("librewolf-config-link", "click", openAboutConfig);
-
-    // Notify observers that the UI is now ready
-    Services.obs.notifyObservers(window, "librewolf-pane-loaded");
+Preferences.addSetting({
+  id: "librewolfExtensionUpdateEnabled",
+  pref: "extensions.update.enabled",
+});
+Preferences.addSetting({
+  id: "librewolfExtensionAutoUpdateEnabled",
+  pref: "extensions.update.autoUpdateDefault",
+});
+Preferences.addSetting({
+  id: "librewolfExtensionUpdate",
+  deps: ["librewolfExtensionUpdateEnabled","librewolfExtensionAutoUpdateEnabled"],
+  get: (_, deps) => deps.librewolfExtensionUpdateEnabled.value && deps.librewolfExtensionAutoUpdateEnabled.value,
+  set: (value, deps) => {
+      deps.librewolfExtensionUpdateEnabled.value = value;
+      deps.librewolfExtensionAutoUpdateEnabled.value = value;
   },
-};
+});
 
-function setXOriginPolicySyncListeners(checkboxid, pref, onVals, offVals) {
-  setSyncFromPrefListener(checkboxid, () => onVals.includes(getPref(pref)));
-  setSyncToPrefListener(checkboxid, () =>
-    writeGenericPrefs([pref], [2], [0], document.getElementById(checkboxid).checked)
-  );
-  Preferences.get(pref).on("change", () =>
-    makeMasterCheckboxesReactive(checkboxid, () =>
-      onVals.includes(getPref(pref))
-    )
-  );
-}
+Preferences.addSetting({
+  id: "librewolfSync",
+  pref: "identity.fxaccounts.enabled",
+  onUserChange() {
+    confirmRestartPrompt(
+      Services.prefs.getBoolPref("identity.fxaccounts.enabled"),
+      1,
+      true,
+      false
+    ).then(buttonIndex => {
+      if (buttonIndex == CONFIRM_RESTART_PROMPT_RESTART_NOW) {
+          Services.startup.quit(
+            Ci.nsIAppStartup.eAttemptQuit | Ci.nsIAppStartup.eRestart
+          );
+          return
+        }
+    });
+  }
+});
+
+Preferences.addSetting({
+  id: "librewolfAutocopy",
+  pref: "clipboard.autocopy",
+});
+Preferences.addSetting({
+  id: "librewolfPaste",
+  pref: "middlemouse.paste",
+});
+Preferences.addSetting({
+  id: "librewolfMiddleClick",
+  deps: ["librewolfAutocopy","librewolfPaste"],
+  get: (_, deps) => deps.librewolfAutocopy.value && deps.librewolfPaste.value,
+  set: (value, deps) => {
+      deps.librewolfAutocopy.value = value;
+      deps.librewolfPaste.value = value;
+  },
+});
+
+Preferences.addSetting({
+  id: "librewolfUserChrome",
+  pref: "toolkit.legacyUserProfileCustomizations.stylesheets",
+});
+
+Preferences.addSetting({
+  id: "librewolfIPv6",
+  pref: "network.dns.disableIPv6",
+  get: (value) => value.value = !value,
+  set: (value) => value.value = !value,
+});
+
+Preferences.addSetting({
+  id: "librewolfCrossOrigin",
+  pref: "network.http.referer.XOriginPolicy",
+  get: (value) => {
+    if (value == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  set: (value) => value ? 2 : 0,
+});
+
+Preferences.addSetting({
+  id: "librewolfRFP",
+  pref: "privacy.resistFingerprinting",
+});
+Preferences.addSetting({
+  id: "librewolfLetterboxing",
+  pref: "privacy.resistFingerprinting.letterboxing",
+});
+
+Preferences.addSetting({
+  id: "librewolfWebGLPrompt",
+  pref: "librewolf.webgl.prompt",
+  get: (value) => value.value = !value,
+  set: (value) => value.value = !value,
+});
+Preferences.addSetting({
+  id: "librewolfWebGLPromptHide",
+  pref: "librewolf.webgl.prompt.hide",
+  deps: ["librewolfWebGLPrompt"],
+  disabled: ({librewolfWebGLPrompt}) => {
+    return librewolfWebGLPrompt.value;
+  },
+});
+
 
 function openProfileDirectory() {
   // Get the profile directory.
@@ -177,96 +168,23 @@ function openAboutConfig() {
   window.open("about:config", "_blank");
 }
 
-function setBoolSyncListeners(checkboxid, opts, vals) {
-  setSyncFromPrefListener(checkboxid, () => readGenericBoolPrefs(opts, vals));
-  setSyncToPrefListener(checkboxid, () => writeGenericBoolPrefs(opts, vals, document.getElementById(checkboxid).checked));
-  for (let i = 1; i < opts.length; i++) {
-    Preferences.get(opts[i]).on("change", () => makeMasterCheckboxesReactive(checkboxid, () => readGenericBoolPrefs(opts, vals)));
-  }
-}
-function setSyncListeners(checkboxid, opts, onVals, offVals) {
-  setSyncFromPrefListener(checkboxid, () => readGenericPrefs(opts, onVals, offVals));
-  setSyncToPrefListener(checkboxid, () => writeGenericPrefs(opts, onVals, offVals, document.getElementById(checkboxid).checked));
-  for (let i = 1; i < opts.length; i++) {
-    Preferences.get(opts[i]).on("change", () => makeMasterCheckboxesReactive(checkboxid, () => readGenericPrefs(opts, onVals, offVals)));
-  }
-}
+var gLibrewolfPane = {
+  _pane: null,
 
-function makeMasterCheckboxesReactive(checkboxid, func) {
-  const shouldBeChecked = func();
-  document.getElementById(checkboxid).checked = shouldBeChecked;
-}
+  // called when the document is first parsed
+  init() {
+    this._pane = document.getElementById("paneLibrewolf");
+    initSettingGroup("librewolfBehavior");
+    initSettingGroup("librewolfNetworking");
+    initSettingGroup("librewolfPrivacy");
+    initSettingGroup("librewolfFingerprinting");
 
-// Wrapper function in case something more is required (as I suspected in the first iteration of this)
-function getPref(pref) {
-  const retval = Preferences.get(pref);
-/*  if (retval === undefined) {
-    return defaultValue;
-  } */
-  return retval._value;
-}
-// Returns true if all the preferences in prefs are equal to onVals, false otherwise TODO may need a third array for their default values because mozilla is dumb, after testing though pretty sure this was misinformation being spread by comments in default FF code that has long since been fixed
-function readGenericBoolPrefs(prefs, onVals) {
-  for (let i = 0; i < prefs.length; i++) {
-    if (getPref(prefs[i]) != onVals[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-function writeGenericBoolPrefs(opts, vals, changeToOn) {
-  valsCopy = [...vals];
-  if (!changeToOn) {
-    for (let i = 0; i < vals.length; i++) {
-      valsCopy[i] = !vals[i];
-    }
-  }
-  // Start at 1 because returning sets the last one
-  for (let i = 1; i < vals.length; i++) {
-    Services.prefs.setBoolPref(opts[i], valsCopy[i]);
-  }
-  return valsCopy[0];
-}
+    // Set event listener on open profile directory button
+    setEventListener("librewolf-open-profile", "command", openProfileDirectory);
+    // Set event listener on open about:config button
+    setEventListener("librewolf-config-link", "click", openAboutConfig);
 
-// Returns true if all the preferences in prefs are equal to onVals, false otherwise... currently the same as for Bool as offVals is ignored
-function readGenericPrefs(prefs, onVals, offVals) {
-  for (let i = 0; i < prefs.length; i ++) {
-    let temp = getPref(prefs[i]);
-    if (getPref(prefs[i]) != onVals[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-function writeGenericPrefs(opts, onVals, offVals, changeToOn) {
-  let writeArr = (changeToOn) ? onVals : offVals;
-  for (let i = 1; i < opts.length; i++) {
-    let type = typeof(writeArr[i]);
-    if (type == "number") {
-      Services.prefs.setIntPref(opts[i], writeArr[i]);
-    } else if (type == "boolean") {
-      Services.prefs.setBoolPref(opts[i], writeArr[i]);
-    } else if (type == "string") {
-      Services.prefs.setCharPref(opts[i], writeArr[i]);
-    } else {
-      console.log("BADNESS 10000");
-    }
-  }
-  return writeArr[0];
-}
-
-Preferences.get("identity.fxaccounts.enabled").on("change", () => {
-  confirmRestartPrompt(
-    Services.prefs.getBoolPref("identity.fxaccounts.enabled"), // Restart is required to *enable* / *disable* the pref
-    1, // Default Button Index
-    true, // Cancel instead of Revert Button
-    false // No Restart Later button
-  ).then(buttonIndex => {
-    if (buttonIndex == CONFIRM_RESTART_PROMPT_RESTART_NOW) {
-      Services.startup.quit(
-        Ci.nsIAppStartup.eAttemptQuit | Ci.nsIAppStartup.eRestart
-      );
-      return;
-    }
-  });
-});
+    // Notify observers that the UI is now ready
+    Services.obs.notifyObservers(window, "librewolf-pane-loaded");
+  },
+};
